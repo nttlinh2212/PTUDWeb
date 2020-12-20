1b4011ab96af8583acc4758977223059f26f8bdf
Nguyễn Thành Đạt <52274820+changkho6310@users.noreply.github.com>
2020-12-20 16:28:05 +0700

    (detail , byCat1 , byCat2) version-01

--- OnlineAcademy/middlewares/routes.js
+++ OnlineAcademy/middlewares/routes.js
@@ -1,17 +1,12 @@
-var indexRouter = require('../routes/index');
-var usersRouter = require('../routes/users');
-var courseRouter = require('../routes/course');
-
 module.exports = function (app) {
-    app.use('/', indexRouter);
-    app.use('/users', usersRouter);
-    app.use('/course', courseRouter);
+    app.use('/', require('../routes/index'));
+    app.use('/users', require('../routes/users'));
+    app.use('/course', require('../routes/course'));
     
     
     
     // catch 404 and forward to error handler
     app.use(function(req, res) {
-      //next(createError(404));
       res.render('404',{title: "404"});
     });
     // error handler
@@ -23,6 +18,6 @@ module.exports = function (app) {
         console.log(err);
         // render the error page
         res.status(err.status || 500);
-        res.render('500',{title: "505"});
+        // res.render('500',{title: "505"});
     });
 }
