60d8fa108f35b807df5b3c1788429228ac2fdd3a
thanhvyhcmus <thanhvy.forwork@gmail.com>
2020-12-21 20:54:31 +0700

    login - register

--- OnlineAcademy/routes/account.js
+++ OnlineAcademy/routes/account.js
@@ -11,118 +11,114 @@ const secret = speakeasy.generateSecret({ length: 20 });
 
 
 const router = express.Router();
-  router.get('/register', function (req, res, next) {
+router.get('/register', function(req, res, next) {
     res.render('account/register');
-  })
-  
-  router.post('/register', async function (req, res, next) {
+})
+
+router.post('/register', async function(req, res, next) {
     const hash = bcrypt.hashSync(req.body.password, 10);
     //const dob = moment(req.body.dob, 'DD/MM/YYYY').format('YYYY-MM-DD');
     const user = {
-      email: email,
-      password: hash,
-      fullname: req.body.fullname,
-      permission: 0
-      //isVerified: 0
+        email: email,
+        password: hash,
+        fullname: req.body.fullname,
+        permission: 0
+            //isVerified: 0
     }
     req.session.registedUser = user;
 
     //await userModel.add(user);
-    res.render('account/verify',{title:'Verify An Email',user});
-  })
-  router.get('/is-available', async function (req, res) {
+    res.render('account/verify', { title: 'Verify An Email', user });
+})
+router.get('/is-available', async function(req, res) {
     const email = req.session.registedUser.email;
     const user = await userModel.findOneByEmail(email);
     if (user === null) {
-      return res.json(true);
+        return res.json(true);
     }
     res.json(false);
-  })
-  
-  router.get('/send-otp', async function (req, res) {
-    const email = req.session.registedUser.email;; 
+})
+
+router.get('/send-otp', async function(req, res) {
+    const email = req.session.registedUser.email;;
     var token = speakeasy.totp({
-      secret: secret.base32,
-      encoding: 'base32',
-      step: 60
+        secret: secret.base32,
+        encoding: 'base32',
+        step: 60
     });
     var transporter = nodemailer.createTransport({ service: 'Gmail', auth: { user: 'emailforptudw@gmail.com', pass: 'Ptudw2020' } });
-    var mailOptions = { from: 'emailforptudw@gmail.com', to: email, subject: 'Account Verification Code', text: 'Your verification code is: '+ token };
-    transporter.sendMail(mailOptions, function (err) {
-      if (err) { 
-        console.log(err);
-        res.json(false);//'Technical Issue!, Please click on resend for verify your Email.'
-        
-      }
-      else{
-        console.log('A verification email has been sent to ' + email + '. It will be expire after 60s. If you do not recieve, please click resend '+ token);
-        res.json(true);
-      }   
+    var mailOptions = { from: 'emailforptudw@gmail.com', to: email, subject: 'Account Verification Code', text: 'Your verification code is: ' + token };
+    transporter.sendMail(mailOptions, function(err) {
+        if (err) {
+            console.log(err);
+            res.json(false); //'Technical Issue!, Please click on resend for verify your Email.'
+
+        } else {
+            console.log('A verification email has been sent to ' + email + '. It will be expire after 60s. If you do not recieve, please click resend ' + token);
+            res.json(true);
+        }
     });
-  })
-  router.post('/verify', async function (req, res, next) {
+})
+router.post('/verify', async function(req, res, next) {
     const token = req.body.code;
     var tokenValidates = speakeasy.totp.verify({
-      secret: secret.base32,
-      encoding: 'base32',
-      token,
-      window: 0,
-      step: 60
+        secret: secret.base32,
+        encoding: 'base32',
+        token,
+        window: 0,
+        step: 60
     });
-    if (tokenValidates)
-    {
-      console.log('is Verified');
-      await userModel.add(req.session.registedUser);
-      req.session.registedUser = null;
-      res.redirect('account/login');
+    if (tokenValidates) {
+        console.log('is Verified');
+        await userModel.add(req.session.registedUser);
+        req.session.registedUser = null;
+        res.redirect('account/login');
+    } else {
+        console.log('wrong code');
+        res.redirect(req.headers.referer || '/');
     }
-      
-    else{
-      console.log('wrong code');
-      res.redirect(req.headers.referer || '/');
-    }
-      
-    
+
+
     ///Note direct...
-  })
-  
-  router.get('/login', function (req, res) {
+})
+
+router.get('/login', function(req, res) {
     res.render('account/login', {
-      layout: false
+        layout: false
     });
-  })
-  
-  router.post('/login', async function (req, res) {
+})
+
+router.post('/login', async function(req, res) {
     const user = await userModel.findOneByEmail(req.body.email);
     if (user === null) {
-      return res.render('account/login', {
-        layout: false,
-        err_message: 'Invalid username.'
-      });
+        return res.render('account/login', {
+            layout: false,
+            err_message: 'Invalid username.'
+        });
     }
-  
+
     const ret = bcrypt.compareSync(req.body.password, user.password);
     if (ret === false) {
-      return res.render('account/login', {
-        layout: false,
-        err_message: 'Invalid password.'
-      });
+        return res.render('account/login', {
+            layout: false,
+            err_message: 'Invalid password.'
+        });
     }
-    
+
     req.session.auth = true;
     req.session.authUser = user;
-  
+
     const url = req.session.retUrl || '/';
     res.redirect(url);
-  })
-  
-  router.post('/logout', async function (req, res) {
+})
+
+router.post('/logout', async function(req, res) {
     req.session.auth = false;
     req.session.authUser = null;
     req.session.retUrl = null;
-  
+
     const url = req.headers.referer || '/';
     res.redirect(url);
-  })
-  
-  module.exports = router;
+})
+
+module.exports = router;
