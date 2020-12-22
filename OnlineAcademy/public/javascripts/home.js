79abaf315df78f497b7eeaba53b1f120da1d8eab
thanhvyhcmus <thanhvy.forwork@gmail.com>
2020-12-22 22:34:32 +0700

    home v1.2

--- OnlineAcademy/public/javascripts/home.js
+++ OnlineAcademy/public/javascripts/home.js
@@ -1,15 +1,15 @@
 $("#reccomended").owlCarousel({
     loop: true,
     autoplay: true,
-    autoplayTimeout: 5000,
+    autoplayTimeout: 7000,
 })
 $("#top10CoursesByViews").owlCarousel({
     items: 4,
     loop: true,
     margin: 10,
     autoplay: true,
-    autoplayTimeout: 5000,
-    autoplayHoverPause: false,
+    autoplayTimeout: 7000,
+    autoplayHoverPause: true,
     responsiveClass: true,
     navText: ["<i class='fa fa-chevron-left'></i>", "<i class='fa fa-chevron-right'></i>"],
     responsive: {
@@ -34,7 +34,7 @@ $("#top10NewCourses").owlCarousel({
     margin: 10,
     autoplay: true,
     autoplayTimeout: 5000,
-    autoplayHoverPause: false,
+    autoplayHoverPause: true,
     responsiveClass: true,
     navText: ["<i class='fa fa-chevron-left'></i>", "<i class='fa fa-chevron-right'></i>"],
     responsive: {
@@ -52,3 +52,7 @@ $("#top10NewCourses").owlCarousel({
         }
     }
 });
+
+function calStar(array) {
+    return round(array[0] + array[1] * 2 + array[2] * 3 + array[3] * 4 + array[4] * 5);
+}
