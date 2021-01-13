cc827aaabd8608b9ef5f5e260f23076ace57e3d6
thanhvyhcmus <thanhvy.forwork@gmail.com>
2021-01-13 10:30:45 +0700

    add to cart animation

--- OnlineAcademy/public/javascripts/header.js
+++ OnlineAcademy/public/javascripts/header.js
@@ -45,20 +45,20 @@ window.onclick = function(event) {
     }
 }
 
-$(document).ready(function() {
-    $('#addtocart').on('click', function() {
-        var button = $(this);
-        var cart = $('#cart');
-        var cartTotal = cart.attr('data-totalitems');
-        var newCartTotal = parseInt(cartTotal) + 1;
+// $(document).ready(function() {
+//     $('#addself').on('click', function() {
+//         var button = $(this);
+//         var cart = $('#cart');
+//         var cartTotal = cart.attr('data-totalitems');
+//         var newCartTotal = parseInt(cartTotal) + 1;
 
-        button.addClass('sendtocart');
-        setTimeout(function() {
-            button.removeClass('sendtocart');
-            cart.addClass('shake').attr('data-totalitems', newCartTotal);
-            setTimeout(function() {
-                cart.removeClass('shake');
-            }, 500)
-        }, 1000)
-    })
-})
+//         button.addClass('sendtocart');
+//         setTimeout(function() {
+//             button.removeClass('sendtocart');
+//             cart.addClass('shake').attr('data-totalitems', newCartTotal);
+//             setTimeout(function() {
+//                 cart.removeClass('shake');
+//             }, 500)
+//         }, 1000)
+//     })
+// })
