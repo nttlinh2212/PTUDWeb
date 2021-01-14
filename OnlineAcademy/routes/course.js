3cb0a685c81522603f332073f39e839d60b03a66
Nguyễn Thành Đạt <52274820+changkho6310@users.noreply.github.com>
2021-01-14 20:51:14 +0700

    student review course (not yet : need to fix models)

--- OnlineAcademy/routes/course.js
+++ OnlineAcademy/routes/course.js
@@ -7,14 +7,14 @@ var lessionModel = require('../models/lession')
 var userModel = require('../models/user')
 const { getCurrency, getStar, getDayLeft, getSecond } = require('../utils/helpers');
 const moment = require('moment');
-var findCourseModel = require('../models/course');
+var findCourseModel = require('../models/findCourse');
 
 
 const totalItemsPerPage = 3;
 const numberOfPageShowing = 3;
 
 
-router.get('/byCat1/:id/:currentPage', async function(req, res, next) {
+router.get('/byCat1/:id/:currentPage', async function (req, res, next) {
     const Cat1ID = +req.params.id;
     var lstCourses = await courseModel.allCoursesByCategory1(Cat1ID);
     var numberOfPage = lstCourses.length / totalItemsPerPage;
@@ -59,7 +59,7 @@ router.get('/byCat1/:id/:currentPage', async function(req, res, next) {
 });
 
 
-router.get('/byCat1/:id/', async function(req, res, next) {
+router.get('/byCat1/:id/', async function (req, res, next) {
     const Cat1ID = +req.params.id;
     var lstCourses = await courseModel.allCoursesByCategory1(Cat1ID);
     var numberOfPage = lstCourses.length / totalItemsPerPage;
@@ -88,7 +88,7 @@ router.get('/byCat1/:id/', async function(req, res, next) {
 
 
 
-router.get('/byCat2/:id/:currentPage', async function(req, res, next) {
+router.get('/byCat2/:id/:currentPage', async function (req, res, next) {
     const Cat2ID = +req.params.id;
     var lstCourses = await courseModel.allCoursesByCategory2(Cat2ID);
     var numberOfPage = lstCourses.length / totalItemsPerPage;
@@ -134,7 +134,7 @@ router.get('/byCat2/:id/:currentPage', async function(req, res, next) {
 });
 
 
-router.get('/byCat2/:id', async function(req, res, next) {
+router.get('/byCat2/:id', async function (req, res, next) {
     const Cat2ID = +req.params.id;
     var lstCourses = await courseModel.allCoursesByCategory2(Cat2ID);
     var numberOfPage = lstCourses.length / totalItemsPerPage;
@@ -164,14 +164,14 @@ router.get('/byCat2/:id', async function(req, res, next) {
 });
 
 
-router.get('/detail/:id', async function(req, res, next) {
+router.get('/detail/:id', async function (req, res, next) {
 
     const CourseID = +req.params.id;
 
     courseModel.updateViews(CourseID);
     const top5courses = await courseModel.top5TheSameCategory1CoursesBuy(CourseID);
     const course = await courseModel.findACourse(CourseID);
-    if(course === null)
+    if (course === null)
         res.redirect('/');
     const lecture = await userModel.findALecture(course.LectureID);
     console.log(lecture);
@@ -180,24 +180,24 @@ router.get('/detail/:id', async function(req, res, next) {
     if (req.session.auth === true && await checkAStudenntParticipatingCourse(CourseID, req.session.authUser.UserID) !== null) {
         const lastlession = await getLastLession(CourseID, req.session.authUser.UserID);
         const percentage = await getPercentageCompleting(CourseID, req.session.authUser.UserID);
-        res.render('course/detail1', { layout: false, title: course.title, course, lecture, feedback, getCurrency, getStar, getDayLeft, top5courses, lessions, lastlession, percentage,type:1 });
+        res.render('course/detail1', { layout: false, title: course.title, course, lecture, feedback, getCurrency, getStar, getDayLeft, top5courses, lessions, lastlession, percentage, type: 1 });
 
-    }else if (req.session.auth === true && await checkCourseByLecturer(req.session.authUser.UserID,CourseID) !== null) {
+    } else if (req.session.auth === true && await checkCourseByLecturer(req.session.authUser.UserID, CourseID) !== null) {
         //const lastlession = await getLastLession(CourseID, req.session.authUser.UserID);
         //const percentage = await getPercentageCompleting(CourseID, req.session.authUser.UserID);
-        res.render('course/detail1', { layout: false, title: course.title, course, lecture, feedback, getCurrency, getStar, getDayLeft, top5courses, lessions,type:2 , percentage:0});
+        res.render('course/detail1', { layout: false, title: course.title, course, lecture, feedback, getCurrency, getStar, getDayLeft, top5courses, lessions, type: 2, percentage: 0 });
 
-    }  else if (req.session.auth === true &&+req.session.authUser.type_of_account===2 ) {
+    } else if (req.session.auth === true && +req.session.authUser.type_of_account === 2) {
         //const lastlession = await getLastLession(CourseID, req.session.authUser.UserID);
         //const percentage = await getPercentageCompleting(CourseID, req.session.authUser.UserID);
-        res.render('course/detail1', { layout: false, title: course.title, course, lecture, feedback, getCurrency, getStar, getDayLeft, top5courses, lessions,type:2 , percentage:0});
+        res.render('course/detail1', { layout: false, title: course.title, course, lecture, feedback, getCurrency, getStar, getDayLeft, top5courses, lessions, type: 2, percentage: 0 });
 
-    }  
+    }
     else
         res.render('course/detail', { layout: false, title: course.title, course, lecture, feedback, getCurrency, getStar, getDayLeft, top5courses, lessions, isStudent: req.session.auth });
 });
 
-router.get('/get-last-point-time', async function(req, res, next) {
+router.get('/get-last-point-time', async function (req, res, next) {
     const LessionID = req.query.lessionid;
     console.log(LessionID);
     const history = await findALessionHistory(LessionID, req.session.authUser.UserID);
@@ -208,11 +208,11 @@ router.get('/get-last-point-time', async function(req, res, next) {
         return res.json(0);
 
 });
-router.get('/get-feed-back', async function(req, res, next) {
+router.get('/get-feed-back', async function (req, res, next) {
     console.log(req.query);
-    
+
     const feedback = {
-        StudentID : req.session.authUser.UserID,
+        StudentID: req.session.authUser.UserID,
         CourseID: req.query.CourseID,
         star: req.query.star,
         review: req.query.comment,
@@ -226,28 +226,28 @@ router.get('/get-feed-back', async function(req, res, next) {
             break;
         case 2:
             await courseModel.updateStar2(req.query.CourseID);
-            break; 
+            break;
         case 3:
             await courseModel.updateStar3(req.query.CourseID);
             break;
         case 4:
             await courseModel.updateStar4(req.query.CourseID);
-            break;     
+            break;
         case 5:
             await courseModel.updateStar5(req.query.CourseID);
-            break; 
+            break;
         default:
             break;
     }
 
-    await findCourseModel.updateParticipating(feedback);//lun true
-    res.json("TRUE");
+    // await findCourseModel.updateParticipating(feedback);//lun true
+    res.json({ result: "true" });
 });
 
 
 
 //update-last-point-time?lessionid=1&lastpoint=78
-router.get('/update-last-point-time', async function(req, res, next) {
+router.get('/update-last-point-time', async function (req, res, next) {
     console.log(req.query);
     entity = {
         lessionid: req.query.lessionid,
@@ -272,7 +272,7 @@ router.get('/update-last-point-time', async function(req, res, next) {
 
 });
 //update-last-watch-lession?lessionid=&courseid
-router.get('/update-last-watch-lession', async function(req, res, next) {
+router.get('/update-last-watch-lession', async function (req, res, next) {
     const { lessionid, courseid } = req.query;
     //console.log(req.query);   
     return res.json(await updateLastLession(lessionid, courseid, req.session.authUser.UserID));
