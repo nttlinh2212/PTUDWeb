0d7aaf6e00768faa07cd80a499ae449da66c57de
Nguyễn Thành Đạt <52274820+changkho6310@users.noreply.github.com>
2021-01-15 00:13:12 +0700

    Admin (filter course by category + lecturer) : DONE

--- OnlineAcademy/routes/admin/course.js
+++ OnlineAcademy/routes/admin/course.js
@@ -2,6 +2,21 @@ const express = require('express');
 const courseModel = require('../../models/course');
 const { update, allCoursesByCatAndLecturer } = require('../../models/findCourse');
 const router = express.Router();
+const categoryModel = require('../../models/category');
+const userModel = require('../../models/user');
+
+
+function getAllStuOrLecOrAd(userList, classify) {
+  var resultList = [];
+
+  userList.forEach(function (item) {
+    if (item.type_of_account === classify) {
+      resultList.push(item);
+    }
+  })
+  console.log(resultList);
+  return resultList;
+}
 
 
 
@@ -13,14 +28,19 @@ router.get('/', async function (req, res) {
 })
 
 
-router.get('/get-courses', async function (req, res) {
+router.get('/get-courses-cat1-lecturer', async function (req, res) {
   const listCourses = await courseModel.all();
+  const listCat1 = await categoryModel.allCat1();
+  const list = await userModel.allStuAndLecturer();
+  var lecturerList = getAllStuOrLecOrAd(list, 1);
+
+
   // console.log(listCourses);
   // listCourses.forEach(function(item){
   //   console.log("______________");
   //   console.log(item.CourseID + "-" + item.disable + "-" + item.title);
   // })
-  res.json(listCourses);
+  res.json({ listCourses, listCat1, lecturerList });
 })
 
 
@@ -40,7 +60,7 @@ router.get('/delete-course', async function (req, res, next) {
 router.get('/disable-course', async function (req, res, next) {
   console.log(req.query);
   const CourseID = req.query.CourseID;
-  const course ={
+  const course = {
     CourseID,
     disable: 1,
   }
@@ -50,7 +70,7 @@ router.get('/disable-course', async function (req, res, next) {
 router.get('/enable-course', async function (req, res, next) {
   console.log(req.query);
   const CourseID = req.query.CourseID;
-  const course ={
+  const course = {
     CourseID,
     disable: 0,
   }
@@ -60,11 +80,39 @@ router.get('/enable-course', async function (req, res, next) {
 
 router.get('/get-courses-filter', async function (req, res) {
   //cat1, cat2, lecturer = null nghia la chon het
-  const test ={Cat1ID:null, Cat2ID:1,LecturerID: null};
-  if(test.Cat1ID===null&&test.Cat2ID===null&&test.LectureID===null)
-    return res.json(await courseModel.all());
-  const listCourses = await allCoursesByCatAndLecturer(test.Cat1ID,test.Cat2ID,test.LecturerID);
-  res.json(listCourses);
+  var Cat1ID = req.query.Cat1ID;
+  var LectureID = req.query.UserID;
+  if (Cat1ID === '0') {
+    Cat1ID = null;
+  }
+  if (LectureID === '0') {
+    LectureID = null;
+  }
+  var filter = {
+    Cat1ID,
+    Cat2ID: null,
+    LectureID
+  }
+  console.log(filter);
+
+  if (filter.Cat1ID === null && filter.Cat2ID === null && filter.LectureID === null){
+    const listCourses = await courseModel.all();
+    console.log(listCourses);
+    res.json(listCourses);
+  }
+  else{
+    const listCourses = await allCoursesByCatAndLecturer(filter.Cat1ID, filter.Cat2ID, filter.LectureID);
+    // console.log("_________________________________________________________________________");
+    // console.log("_________________________________________________________________________");
+    // console.log("_________________________________________________________________________");
+    // console.log("_________________________________________________________________________");
+    // console.log("_________________________________________________________________________");
+    // console.log("_________________________________________________________________________");
+    // console.log("_________________________________________________________________________");
+    // console.log("_________________________________________________________________________");
+    console.log(listCourses);
+    res.json(listCourses);
+  }
 })
 
 
