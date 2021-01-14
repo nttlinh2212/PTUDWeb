5327cb8c9fe39d16725ceb4abb37d5703cf2468d
Nguyễn Thành Đạt <52274820+changkho6310@users.noreply.github.com>
2021-01-14 22:29:50 +0700

    admin (block student, lecturer) : DONE, admin(block course) waiting for fixing model

--- OnlineAcademy/routes/admin/course.js
+++ OnlineAcademy/routes/admin/course.js
@@ -15,6 +15,11 @@ router.get('/', async function (req, res) {
 
 router.get('/get-courses', async function (req, res) {
   const listCourses = await courseModel.all();
+  // console.log(listCourses);
+  // listCourses.forEach(function(item){
+  //   console.log("______________");
+  //   console.log(item.CourseID + "-" + item.disable + "-" + item.title);
+  // })
   res.json(listCourses);
 })
 
@@ -66,68 +71,5 @@ router.get('/get-courses-filter', async function (req, res) {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-router.get('/edit', async function (req, res) {
-  const id = req.query.id;
-  const category = await categoryModel.findCat1(id);
-  if (category === null) {
-    return res.redirect('/admin/cat1');
-  }
-
-  res.render('admin/cat1/edit', {
-    category
-  });
-})
-
-router.get('/add', function (req, res) {
-  res.render('admin/cat1/add');
-})
-
-router.post('/add', async function (req, res) {
-  await categoryModel.addCat1(req.body);
-  res.render('/admin/cat1');
-})
-
-router.post('/del', async function (req, res) {
-  await categoryModel.delCat1(req.body.CatID);
-  res.redirect('/admin/cat1');
-})
-
-router.post('/update', async function (req, res) {
-  await categoryModel.updateCat1(req.body);
-  res.redirect('/admin/cat1');
-})
 
 module.exports = router;
